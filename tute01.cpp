407c47cd671a97b79c8361c58148c0e0312d0632
it21479250 <99161933+it21479250@users.noreply.github.com>
2022-02-14 04:12:09 +0000

    still edign

--- tute01.cpp
+++ tute01.cpp
@@ -18,13 +18,28 @@ printf("Hello World") --> std::cout << "Hello World";
 */
 
 
-#include <stdio.h>
+#include <iostream>
 int main() 
 {
     float cm, inches;
-    printf("Enter a length in cm : ");
+    cout<<"Enter a length in cm : ";
     scanf("%f",&cm);
     inches = cm / 2.54;
-    printf("Length in inches is %f \n", inches);
+    std::cout<<"Length in inches is "<< inches << std::endl;
+    return 0;
+}  
+
+#include <iostream>
+int main() 
+{
+    float cm, inches;
+    using namespace std;
+
+    cout<<"Enter a length in cm : ";
+    cin>>cm;
+
+    inches = cm / 2.54;
+    cout<<"Length in inches is "<< inches << endl;
+
     return 0;
 }  
--- tute02.cpp
+++ tute02.cpp
@@ -12,16 +12,16 @@ Type = 3
 OtRate = 1700
 Please Note that the input command in C++ is std::cin. This is a representation of the Keyboard.*/
 
-#include <stdio.h>
+#include <iostream>
 int main()
 {
    double salary, netSalary;
    int etype, otHrs, otRate;
-   printf("Enter Employee Type : ");
+   cout<<"Enter Employee Type : ";
    scanf("%d", &etype);
-   printf("Enter Salary  : ");
+   cout<<"Enter Salary  : ";
    scanf("%f", &salary);
-   printf("Enter OtHrs : ");
+   cout<<"Enter OtHrs : ";
    scanf("%d", &otHrs); 
    
    switch (etype) {
@@ -38,7 +38,7 @@ int main()
 
 
    netSalary = salary + otHrs* otRate;
-   printf("Net Salary is %f ", netSalary);
+   std::cout<<"Net Salary is " << netSalary;
   
    return 0;
 }
